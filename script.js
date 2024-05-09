ee2c9dfb6aca2e0e61e9457987f5f60b408f3b94
KashviS24 <sherk758@newschool.edu>
2024-05-09 01:52:53 -0400

    update to check drag feature on phone

--- script.js
+++ script.js
@@ -82,15 +82,6 @@ myHome.addEventListener('click', function() {
     window.location.href = "index.html";
 });
 
-document.addEventListener('DOMContentLoaded', function() {
-    const myInfo = document.getElementById("myInfo");
-
-    // Add click event listener
-    myInfo.addEventListener('click', function() {
-        window.location.href = "info.html";
-    });
-});
-
 let currentlyDraggedElement = null;
 
 // Make images draggable
@@ -100,11 +91,12 @@ document.querySelectorAll('.draggable-note-1').forEach(function(image) {
 
 function makeDraggable(element) {
     let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-    element.onmousedown = dragMouseDown;
-    element.onmouseup = function() { currentlyDraggedElement = null; };
+    element.addEventListener('mousedown', dragMouseDown);
+    element.addEventListener('touchstart', dragTouchStart, { passive: false });
+    element.addEventListener('mouseup', function() { currentlyDraggedElement = null; });
+    element.addEventListener('touchend', function() { currentlyDraggedElement = null; });
 
     function dragMouseDown(e) {
-        e = e || window.event;
         e.preventDefault();
         // Set the currently dragged element
         currentlyDraggedElement = element;
@@ -113,19 +105,32 @@ function makeDraggable(element) {
         // get the mouse cursor position at startup:
         pos3 = e.clientX;
         pos4 = e.clientY;
-        document.onmouseup = closeDragElement;
+        document.addEventListener('mouseup', closeDragElement);
         // call a function whenever the cursor moves:
-        document.onmousemove = elementDrag;
+        document.addEventListener('mousemove', elementDrag);
+    }
+
+    function dragTouchStart(e) {
+        e.preventDefault();
+        // Set the currently dragged element
+        currentlyDraggedElement = element;
+        // Increase z-index to make the dragged element appear above others
+        element.style.zIndex = 1000;
+        // get the touch position at startup:
+        pos3 = e.touches[0].clientX;
+        pos4 = e.touches[0].clientY;
+        document.addEventListener('touchend', closeDragElement);
+        // call a function whenever the touch position moves:
+        document.addEventListener('touchmove', elementDrag, { passive: false });
     }
 
     function elementDrag(e) {
-        e = e || window.event;
         e.preventDefault();
-        // calculate the new cursor position:
-        pos1 = pos3 - e.clientX;
-        pos2 = pos4 - e.clientY;
-        pos3 = e.clientX;
-        pos4 = e.clientY;
+        // calculate the new cursor/touch position:
+        pos1 = pos3 - (e.clientX || e.touches[0].clientX);
+        pos2 = pos4 - (e.clientY || e.touches[0].clientY);
+        pos3 = (e.clientX || e.touches[0].clientX);
+        pos4 = (e.clientY || e.touches[0].clientY);
         // set the element's new position:
         element.style.top = (element.offsetTop - pos2) + "px";
         element.style.left = (element.offsetLeft - pos1) + "px";
@@ -136,27 +141,10 @@ function makeDraggable(element) {
         element.style.zIndex = '';
         // Clear the currently dragged element reference
         currentlyDraggedElement = null;
-        // stop moving when mouse button is released:
-        document.onmouseup = null;
-        document.onmousemove = null;
+        // stop moving when mouse/touch interaction ends:
+        document.removeEventListener('mouseup', closeDragElement);
+        document.removeEventListener('touchend', closeDragElement);
+        document.removeEventListener('mousemove', elementDrag);
+        document.removeEventListener('touchmove', elementDrag);
     }
 }
-
-// Add click event listener to all draggable elements
-document.querySelectorAll('.draggable-note-1').forEach(function(element) {
-    element.addEventListener('click', function() {
-        // Set the clicked element as the currently dragged element
-        currentlyDraggedElement = element;
-        // Increase z-index to make the clicked element appear above others
-        element.style.zIndex = 1000;
-    });
-});
-
-// Add a click event listener to the document to clear the currently dragged element
-document.addEventListener('click', function(e) {
-    // If the click is not on a draggable element, clear the currently dragged element
-    if (!e.target.classList.contains('draggable-note-1') && currentlyDraggedElement) {
-        currentlyDraggedElement.style.zIndex = '';
-        currentlyDraggedElement = null;
-    }
-});
